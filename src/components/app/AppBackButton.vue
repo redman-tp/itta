88b8b1abddd026905b993a0b4d8a3c86afd55256
redman-tp <withneywayne@gmail.com>
2026-02-07 23:14:35 +0100

    feat: add reusable floating back button component

---
+++ src/components/app/AppBackButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-btn
+    round
+    unelevated
+    icon="chevron_left"
+    aria-label="Go back"
+    class="itta-back-btn"
+    @click="$emit('click')"
+  />
+</template>
+
+<script setup lang="ts">
+defineEmits<{ (e: 'click'): void }>();
+</script>
